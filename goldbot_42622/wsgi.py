56ad5f686d40f0f111584d596acf82f1eb3196fe
Crowdbotics <team@crowdbotics.com>
2023-08-03 18:59:34 +0000

    Initial commit.

---
+++ goldbot_42622/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for goldbot_42622 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'goldbot_42622.settings')
+
+application = get_wsgi_application()
